82f280899ba91d50b389bda74212df7eaf41d6a3
Adam Kumlin <skola.adamkumlin99@gmail.com>
2025-01-08 15:05:13 +0100

    feat: Add sample components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import Logo from "../assets/health_care_logo.svg";
 import { Link } from "react-router-dom";
-import { Button, buttonVariants } from "@/components/ui/button.jsx"
+import { Button } from "../components/ui/button"
+import { Switch } from "../components/ui/switch"
+
 export default function Home() {
   return (
     <>
       <div>
-        <Button variant={buttonVariants} >Test</Button>
+        <Button variant="default">Test</Button>
+        <Switch />
         <img src={Logo} />
         <h1>Health Care Appointment App</h1>
         <button>
