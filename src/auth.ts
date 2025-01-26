cce85f98caf6007cd83413e9af1fdbcb6d7e68ef
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2025-01-26 13:57:34 +0100

    feat: first cleaned up version of auth config

--- src/auth.ts
+++ src/auth.ts
@@ -4,26 +4,27 @@ import Credentials from "next-auth/providers/credentials"
 //import { URL } from "url" // url object is disallowed when using next-auth due to problems with edge runtime
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: process.env.NODE_ENV !== "production", // allow debug messages in console in dev mode
   providers: [
     Credentials({
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
       credentials: {
-        username: {},
-        password: {},
+        Username: {},
+        Password: {},
       },
       authorize: async (credentials) => {
-        let user = null; // set here so that we explicitly have to set it
+        let user = null; // set to null here so that we explicitly have to set it
         
         // TODO - change this before production so env variable points to correct secret
         const baseUrl: string | undefined = process.env.BACKEND_API_HTTP;
 
-        if (!baseUrl) {
+        if (!baseUrl) { // this should realistically never happen
           throw new Error("Could not retrieve env variable 'BACKEND_API'");
         }
 
         const requestUrl: string = (baseUrl as string).concat("/Auth/login");
-        console.log(requestUrl);
+
         try {
           const response = await axios.post(requestUrl.toString(), credentials);
 
@@ -32,79 +33,19 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           }
 
           user = response.data;
+
         } catch (error) {
           throw new Error(error + "");
         }
 
-        if (!user) {
-          throw new Error("Invalid credentials."); // No user found
+        if (!user) { // No user found
+          throw new Error("Invalid credentials.");
         }
 
         // return user object with their profile data
+        // (this object might need to be extended to accomodate our use-case)
         return user;
       },
     }),
   ],
 })
-
-// export const { handlers, signIn, signOut, auth } = NextAuth({
-//   session: { strategy: "jwt" },
-//   providers: [
-//     Credentials({
-//       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-//       // e.g. domain, username, password, 2FA token, etc.
-//       name: "Login",
-//       credentials: {
-//         username: { label: "Username", type: "username" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       authorize: async (credentials) => {
-//         const baseUrl: string | undefined = process.env.BACKEND_API;
-
-//         if (!baseUrl) {
-//           console.error("Could not retrieve env variable 'BACKEND_API'");
-//           return;
-//         }
-
-//         const requestUrl: URL = new URL("/Auth/login", baseUrl);
-
-//         const response = await (async () => {
-//           try {
-//              const axiosResponse = await axios.post(requestUrl.toString(), credentials)
-//             // const resp = await fetch(requestUrl, {
-//             //   method: "POST",
-//             // })
-//             if (!(axiosResponse.status !== 200)) {
-//               throw new Error;
-//             }
-//           } catch {
-//             console.error("Invalid e-mail or password.");
-//             return;
-//           }
-//         });
-
-//         const user = {
-//           token: response
-//         };
-
-//         return user
-//       },
-//     }),
-//   ],
-//   pages: {
-//     signIn: "/Auth/login",
-//   },
-//   callbacks: {
-//     async jwt({ token, account }) {
-//       if (account) {
-//         token.accessToken = account.access_token;
-//       }
-//       return token;
-//     },
-//     async session({ session, token }) {
-//       session.accessToken = token.accessToken;
-//       return session;
-//     }
-//   },
-// })
-
