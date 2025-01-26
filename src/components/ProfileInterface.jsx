e9ace896e46ca67d187a57cefc40f3cc23efd6fc
Sam <zaaamuel_96@hotmail.com>
2025-01-26 20:26:14 +0100

    Created ProfileInterface.jsx

---
+++ src/components/ProfileInterface.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import axios from "axios";
+
+const Profile = () => {
+    // Account details
+    const [account, setAccount] = useState
+}
